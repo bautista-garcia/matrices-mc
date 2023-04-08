b62b9069d33b58db0e5f61b59b9f6836b422d6d6
Bautista <bautax1234@gmail.com>
2023-04-08 12:31:30 -0300

    Version inicial finalizada

--- index.mjs
+++ index.mjs
@@ -20,5 +20,76 @@ function mostrarMatriz(matriz, id) {
   }
 
 let prueba = new Matriz([[1,2], [1, 2]], 2, 2);
-mostrarMatriz(prueba.getMatriz(), 'output');
-mostrarMatriz(prueba.getMatriz(), 'resultado');
+
+
+
+//Event listeners para transportar input de forms a logica de matrices
+const formulario1 = document.getElementById('formulario-1');
+formulario1.addEventListener('submit', (e) => {enviarFormulario(1, e)});
+
+const formulario2 = document.getElementById('formulario-2');
+formulario2.addEventListener('submit', (e) => {enviarFormulario(2, e)});
+
+let matriz1;
+let matriz2;
+
+function enviarFormulario (numForm, e) {
+        e.preventDefault();
+        const columnas = document.getElementById("columnas" + numForm).value;
+        const filas = document.getElementById("filas" + numForm).value;
+        const matriz = document.getElementById("matriz" + numForm).value;
+
+        const matrizVerdadera = obtenerMatriz(filas, columnas, matriz);
+
+        if(numForm == 1) matriz1 =  new Matriz(matrizVerdadera, filas, columnas);
+        else matriz2 = new Matriz(matrizVerdadera, filas, columnas);
+        
+        mostrarMatriz(matrizVerdadera, 'output' + numForm)
+
+        return matrizVerdadera;
+        
+}
+
+
+function obtenerMatriz(n, m, input) {
+  // Convierte el input en un array de números
+  const valores = input.split(' ').map(numero => Number(numero));
+  
+  // Crea una matriz de n filas y m columnas
+  const matriz = Array.from({ length: n }, () => new Array(m));
+  
+  // Llena la matriz con los valores del input
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      matriz[i][j] = valores[i * m + j];
+    }
+  }
+  
+  return matriz;
+}
+
+//Event listeners para las operaciones
+const botonSuma = document.getElementById('suma-btn');
+botonSuma.addEventListener('click', () => {mostrarMatriz(matriz1.sumaMatrices(matriz2.getMatriz(), matriz2.getFilas(), matriz2.getColumnas()), 'resultado')});
+
+const botonMul = document.getElementById('mul-btn');
+botonMul.addEventListener('click', () => {mostrarMatriz(matriz1.multiplicacionMatrices(matriz2), 'resultado')});
+
+
+const invMul1 = document.getElementById('inv1-btn');
+invMul1.addEventListener('click', () => {mostrarMatriz(matriz1.invertir(), 'resultado')});
+
+const invMul2 = document.getElementById('inv2-btn');
+invMul2.addEventListener('click', () => {mostrarMatriz(matriz2.invertir(), 'resultado')});
+
+const botonComp1 = document.getElementById('cmp1-btn');
+botonComp1.addEventListener('click', () => {mostrarMensaje(matriz1.compatibilidad())});
+
+const botonComp2 = document.getElementById('cmp2-btn');
+botonComp2.addEventListener('click', () => {mostrarMensaje(matriz2.compatibilidad())});
+
+
+function mostrarMensaje(mensaje) {
+    const outputDiv = document.getElementById('resultado');
+    outputDiv.textContent = mensaje;
+}
--- logicaMatrices.mjs
+++ logicaMatrices.mjs
@@ -3,7 +3,13 @@ function Matriz  (matriz, filas, columnas)  {
     this.columnas = columnas;
     this.matriz = matriz;
 
-    
+    this.getFilas = () => {
+        return this.filas;
+    }
+
+    this.getColumnas = () => {
+        return this.columnas;
+    }
 
 
     this.getMatriz = () => {
@@ -16,14 +22,20 @@ function Matriz  (matriz, filas, columnas)  {
         return ((this.filas === matriz.filas) && (this.columnas === matriz.columnas));
     }
     
-    this.sumaMatrices = (matrizSuma) => {
-        let matrizResultado = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; //Inicializamos la matriz
-        for(let i = 0; i < this.filas; i++){ //Recorremos filas
-            for(let j = 0; j < this.columnas; j++){ //Recorremos columnas
-                matrizResultado[i][j] = this.matriz[i][j] + matrizSuma[i][j]; 
-            }
+    this.sumaMatrices = (matrizSuma, filas, columnas) => {
+        if(this.getFilas() !== filas || this.getColumnas() !== columnas){
+            window.alert("Las matrices no tienen la misma dimension");
+            return null;
         }
-        return matrizResultado;
+        else{
+            let matrizResultado = matrizSuma;//Inicializamos la matriz
+            for(let i = 0; i < this.filas; i++){ //Recorremos filas
+                for(let j = 0; j < this.columnas; j++){ //Recorremos columnas
+                    matrizResultado[i][j] = this.matriz[i][j] + matrizSuma[i][j]; 
+                }
+            }
+            return matrizResultado;
+        }
     }
 
     this.multiplicacionDefinida = (matriz) => {
@@ -33,7 +45,8 @@ function Matriz  (matriz, filas, columnas)  {
     this.multiplicacionMatrices = (matrizMultiplicacion) => {
         //Comprobamos que las matrices se puedan multiplicar
         if(!this.multiplicacionDefinida(matrizMultiplicacion)){
-            return "No se pueden multiplicar las matrices";
+            Window.alert("No se pueden multiplicar las matrices");
+            return null;
         }
         //Inicializamos la matriz con las filas de this.matriz y columnas de matrizMultiplicacion
         let matrizResultado = new Array(this.filas);
@@ -57,11 +70,29 @@ function Matriz  (matriz, filas, columnas)  {
 
         return matrizResultado;
     }
+    //Metodo para calcular el determinante de una matriz, metodo static
+    
     
     
     this.calcularDeterminante = () => {
-        let res = 0;
-
+        const determinante = (matrizCalc, n) => {
+            //Caso Base si es una matriz de 2x2
+            if(n == 2){
+                return ((matrizCalc[0][0] * matrizCalc[1][1]) - (matrizCalc[0][1] * matrizCalc[1][0]));
+            }
+    
+    
+            let res = 0; //En res se guarda el resultado de cada llamado recursivo
+            let i = 0; //i = 0 porque nos movemos por la fila 0
+    
+            //Formula de serie especificada
+            for(let j = 0; j < n; j++){
+                res += Math.pow(-1, i + j + 2) * matrizCalc[i][j] * determinante(crearSubMatriz(matrizCalc,i,j,n), n - 1);
+            }
+            //Retorno res de llamado recursivo
+            return res; 
+    
+        }
         //Metodo para copiar matriz original a copia, por referencia
         const copiarMatriz = (copia, original, dim) =>{
             for(let i = 0; i < dim; i++){
@@ -93,42 +124,97 @@ function Matriz  (matriz, filas, columnas)  {
             modifMatriz.shift();
             return modifMatriz;    
         }
-        
-        // const crearSubMatriz = (matriz, notFila, notColumna, n) => {  
-        //     //Reservamos espacio de memoria
-        //     let modifMatriz = new Array(n-1);
-        //     for (let i = 0; i < n-1; i++) {
-        //         modifMatriz[i] = new Array(n-1);
-        //     }
-        //     //Copiamos matriz original a la matriz modificada
-        //     for(let i = 0; i < n-1; i++){
-        //         for(let j = 0; j < n-1 ; j ++){
-        //             modifMatriz[i][j] = matriz[i + (i >= notFila)][j + (j >= notColumna)];
-        //         }
-        //     }
-        //     return modifMatriz;    
-        // }
 
-        const determinante = (matrizCalc, n) => {
-            if(n == 2){
-                return ((matrizCalc[0][0] * matrizCalc[1][1]) - (matrizCalc[0][1] * matrizCalc[1][0]));
-            }
-            let res = 0;
-            let i = 0;
-            for(let j = 0; j < n; j++){
-                res += Math.pow(-1, i + j + 2) * matrizCalc[i][j] * determinante(crearSubMatriz(matrizCalc,i,j,n), n - 1);
-            }
-            return res;
-
-        }
-
-        
+        //Retornamos el resultado para que sea accesible al objeto
         let resultado;
         if(this.esCuadrada()){
-            resultado = determinante(this.getMatriz(), this.filas, 0);
+            resultado = determinante(this.getMatriz(), this.filas);
         }
         return resultado;
     }
+
+
+    this.invertir = () => {
+        // Obtener la matriz y sus dimensiones
+        const matriz = this.getMatriz();
+        const filas = this.filas;
+        const columnas = this.columnas;
+
+        // Verificar si la matriz tiene inversa utilizando el método de determinante
+        const det = this.calcularDeterminante();
+        if (det == 0) {
+            Window.alert("La matriz no tiene inversa.");
+            return null;
+        }
+
+        // Construir la matriz ampliada
+        const matrizAmpliada = [];
+        for (let i = 0; i < filas; i++) {
+            matrizAmpliada[i] = [];
+            for (let j = 0; j < columnas * 2; j++) {
+                if (j < columnas) {
+                    matrizAmpliada[i][j] = matriz[i][j];
+                } else {
+                    matrizAmpliada[i][j] = i == j - columnas ? 1 : 0;
+                }
+            }
+        }
+
+        // Aplicar la eliminación gaussiana para obtener la matriz inversa
+        for (let i = 0; i < filas; i++) {
+            const pivot = matrizAmpliada[i][i];
+            for (let j = 0; j < columnas * 2; j++) {
+                matrizAmpliada[i][j] /= pivot;
+            }
+            for (let k = 0; k < filas; k++) {
+                if (k != i) {
+                    const factor = matrizAmpliada[k][i];
+                    for (let j = 0; j < columnas * 2; j++) {
+                        matrizAmpliada[k][j] -= factor * matrizAmpliada[i][j];
+                    }
+                }
+            }
+        }
+
+        // Extraer la matriz inversa de la matriz ampliada
+        const matrizInversa = [];
+        for (let i = 0; i < filas; i++) {
+            matrizInversa[i] = matrizAmpliada[i].slice(columnas);
+        }
+
+        // Retornar la matriz inversa
+        return matrizInversa;
+    } 
+
+    this.compatibilidad = () => {
+        
+
+        //El metodo matrizCoeficientes() crea una matriz con las dimensiones de la matriz original, pero sin la columna de los terminos independientes
+        const matrizCoeficientes = () => {
+            let matrizCoeficientes = new Array(this.filas);
+            for(let i = 0; i < this.filas; i++){
+                matrizCoeficientes[i] = new Array(this.columnas - 1);
+            }
+            for(let i = 0; i < this.filas; i++){
+                for(let j = 0; j < this.columnas - 1; j++){
+                    matrizCoeficientes[i][j] = this.matriz[i][j];
+                }
+            }
+            let objMatrizCoeficientes = new Matriz(matrizCoeficientes, this.filas, this.columnas - 1);
+            return objMatrizCoeficientes;
+        }
+
+        
+
+        if(matrizCoeficientes().calcularDeterminante() == 0){
+            return "El sistema de ecuaciones es incompatible";
+        }
+        else{
+            return "El sistema de ecuaciones es compatible";
+        }
+    }
+        
+    
     
 }
 
